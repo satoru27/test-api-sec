8074469c2894173dcc7c205bdb8f236702e654f9
satoru27 <vitorsatoru@hotmail.com>
2023-08-15 15:05:33 -0400

    Added answer data

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const PORT = 443
 app.listen(PORT, () => console.log('API running on port ' + PORT))
 
 app.get('/', (req,res) => res.json('My API is running :O'))
+
+app.get('/v2/license/security/authorizeToken', (req,res,next) => {
+	let data = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><AuthorizeTokenResultVO><authenticateUserID>kfzd5kaklc</authenticateUserID><authorizeCode>LIC_2101</authorizeCode><authorizeDesc>SUCCESS</authorizeDesc><remainExpireTimeSec>65048</remainExpireTimeSec></AuthorizeTokenResultVO>'
+
+	res.header("Content-Type", "application/xml")
+	res.status(200).send(data)
+})
