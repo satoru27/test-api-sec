e981dd59e77a84987d16c10a19852d777b2bb360
satoru27 <vitorsatoru@hotmail.com>
2023-08-15 11:04:35 -0400

    Port updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const app = express()
-const PORT = 5001 
+const PORT = 443 
 app.listen(PORT, () => console.log('API running on port ' + PORT))
 
 app.get('/', (req,res) => res.json('My API is running :O'))
