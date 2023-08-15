3a5435c924770d05b8d12836e00aa0ffcb603f83
satoru27 <vitorsatoru@hotmail.com>
2023-08-15 15:21:50 -0400

    Added headers

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.get('/', (req,res) => res.json('My API is running :O'))
 app.get('/v2/license/security/authorizeToken', (req,res,next) => {
 	let data = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><AuthorizeTokenResultVO><authenticateUserID>kfzd5kaklc</authenticateUserID><authorizeCode>LIC_2101</authorizeCode><authorizeDesc>SUCCESS</authorizeDesc><remainExpireTimeSec>65048</remainExpireTimeSec></AuthorizeTokenResultVO>'
 
-	res.header("Content-Type", "application/xml")
+	res.removeHeader('X-Powered-By');
+    res.removeHeader('ETag');
+    res.removeHeader('Keep-Alive');
+    res.removeHeader('ETag');
+
+    res.header("Content-Type", "text/xml")
+    res.header("X-RateLimit-Remaining", "100000")
+    res.header("X-RateLimit-Remaining-Quota", "300000000")
+    res.header("X-RateLimit-Quota", "300000000")
+    res.header("X-RateLimit-Reset", "300000")
+    res.header("X-RateLimit-Limit", "100000")
+
 	res.status(200).send(data)
 })
